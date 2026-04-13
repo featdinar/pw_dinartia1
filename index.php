edceb9ffcd1a6708753c66b66bf06ada42117720
featdinar <dinaramelia20@gmail.com>
2026-04-13 12:18:42 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = "Abdull";
+$umur = 20;
+$tinggi = 170.5;
+$menikah = true;
+$hobi = ["membaca", "berenang", "bermain gitar"];
+
+echo "Nama saya $nama,umur saya $, tinggi saya $tinggi cm, status saya $menikah, hobi saya $hobi[1]";
+?>
